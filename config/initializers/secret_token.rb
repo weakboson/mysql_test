a5bf28bfec184076a767c1e4864ecb8ae392f1af
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-01-04 13:39:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MysqlTest::Application.config.secret_key_base = 'ce86b6eceb8f00a974adfe023b9e09e2ce48b7292ad64a9f22508a8aadb8e4401c95d830ae8a37170be3ad328fac789089713448e8c645ed234a079e3a3916ee'
